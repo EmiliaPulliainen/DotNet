ce201dd42ca3eb58812d665c6bb9ec7f78cdab82
Emilia Pulliainen <emilia.m.pulliainen@gmail.com>
2020-09-23 13:39:33 +0300

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numero2 = 0;
+            
+
+            Console.WriteLine("Anna numero:");
+            string numero1 = Console.ReadLine();
+
+            Console.WriteLine("Anna toinen numero:");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            int tulos = int.Parse(numero1) + numero2;
+
+            Console.WriteLine("Tulos on " + tulos);
+        }
+    }
+}
